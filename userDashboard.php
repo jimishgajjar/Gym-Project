5350405036f9996e1876412d1df5a87bfc6c3236
Jimish Gajjar <jimish.gajjar@gmail.com>
2021-10-27 01:05:06 +0530

    Course Tile Design Done

--- userDashboard.php
+++ userDashboard.php
@@ -216,12 +216,115 @@
     <section class="section-login">
         <div class="container">
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-3 mb-25">
                     <div class="course">
+                        <img class="course-thumbline" src="assets/images/course/1.jpg" alt="course1" />
                         <div class="course-content">
-                            <div class="course-title">
+                            <div class="course-title mb-3">
                                 Kubernetes Certified Application Developer (CKAD) with Tests
                             </div>
+                            <div class="course-description mb-3">
+                                Build your foundation in the hottest industry of the 21st century
+                            </div>
+                            <a href="#" class="btn btn-primary">Know More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-25">
+                    <div class="course">
+                        <img class="course-thumbline" src="assets/images/course/1.jpg" alt="course1" />
+                        <div class="course-content">
+                            <div class="course-title mb-3">
+                                Kubernetes Certified Application Developer (CKAD) with Tests
+                            </div>
+                            <div class="course-description mb-3">
+                                Build your foundation in the hottest industry of the 21st century
+                            </div>
+                            <a href="#" class="btn btn-primary">Know More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-25">
+                    <div class="course">
+                        <img class="course-thumbline" src="assets/images/course/1.jpg" alt="course1" />
+                        <div class="course-content">
+                            <div class="course-title mb-3">
+                                Kubernetes Certified Application Developer (CKAD) with Tests
+                            </div>
+                            <div class="course-description mb-3">
+                                Build your foundation in the hottest industry of the 21st century
+                            </div>
+                            <a href="#" class="btn btn-primary">Know More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-25">
+                    <div class="course">
+                        <img class="course-thumbline" src="assets/images/course/1.jpg" alt="course1" />
+                        <div class="course-content">
+                            <div class="course-title mb-3">
+                                Kubernetes Certified Application Developer (CKAD) with Tests
+                            </div>
+                            <div class="course-description mb-3">
+                                Build your foundation in the hottest industry of the 21st century
+                            </div>
+                            <a href="#" class="btn btn-primary">Know More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-25">
+                    <div class="course">
+                        <img class="course-thumbline" src="assets/images/course/1.jpg" alt="course1" />
+                        <div class="course-content">
+                            <div class="course-title mb-3">
+                                Kubernetes Certified Application Developer (CKAD) with Tests
+                            </div>
+                            <div class="course-description mb-3">
+                                Build your foundation in the hottest industry of the 21st century
+                            </div>
+                            <a href="#" class="btn btn-primary">Know More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-25">
+                    <div class="course">
+                        <img class="course-thumbline" src="assets/images/course/1.jpg" alt="course1" />
+                        <div class="course-content">
+                            <div class="course-title mb-3">
+                                Kubernetes Certified Application Developer (CKAD) with Tests
+                            </div>
+                            <div class="course-description mb-3">
+                                Build your foundation in the hottest industry of the 21st century
+                            </div>
+                            <a href="#" class="btn btn-primary">Know More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-25">
+                    <div class="course">
+                        <img class="course-thumbline" src="assets/images/course/1.jpg" alt="course1" />
+                        <div class="course-content">
+                            <div class="course-title mb-3">
+                                Kubernetes Certified Application Developer (CKAD) with Tests
+                            </div>
+                            <div class="course-description mb-3">
+                                Build your foundation in the hottest industry of the 21st century
+                            </div>
+                            <a href="#" class="btn btn-primary">Know More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-25">
+                    <div class="course">
+                        <img class="course-thumbline" src="assets/images/course/1.jpg" alt="course1" />
+                        <div class="course-content">
+                            <div class="course-title mb-3">
+                                Kubernetes Certified Application Developer (CKAD) with Tests
+                            </div>
+                            <div class="course-description mb-3">
+                                Build your foundation in the hottest industry of the 21st century
+                            </div>
+                            <a href="#" class="btn btn-primary">Know More</a>
                         </div>
                     </div>
                 </div>
